681664d560b23029e9878a7b253fc50fb0f3516b
karan-suthar03 <116728069+karan-suthar03@users.noreply.github.com>
2024-01-08 00:28:12 +0530

    Update firbase.init.js

--- actual game/firbase.init.js
+++ actual game/firbase.init.js
@@ -11,5 +11,5 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const game = database.ref('123');
+const game = database.ref('555');
 console.log(game);
